c67efeb1c42830a8af7a428f609cd1fc5a823516
Ben Deverett <deverett@frontend.cluster>
2014-11-07 02:34:50 -0500

    overhaul, much easier to analyze. but probably broke ability to analyze old versions

--- analyze.py
+++ analyze.py
@@ -9,11 +9,9 @@ from scipy.io import savemat,loadmat
 rand = np.random
 pjoin = os.path.join
 
-sim_dir = '/jukebox/wang/deverett/simulations/batch_3'
-out_dir = '/jukebox/wang/abadura/FOR_PAPER_GRAN_CELL/simulations/AFTER_CLUSTER_AND_POSTPROCESSED/batch_3_ANALYZED'
+sim_dir = '/jukebox/wang/deverett/simulations/batch_4'
+out_dir = '/jukebox/wang/abadura/FOR_PAPER_GRAN_CELL/simulations/AFTER_CLUSTER_AND_POSTPROCESSED/batch_4_ANALYZED'
 temp_dir = '/jukebox/wang/deverett/tmp'
-
-
 
 def cat(a,ty):
     if type(a[0]) in [np.ndarray, list]:
@@ -85,6 +83,7 @@ class Result(object):
         self.inn_params = np.atleast_1d(self.inn['params'])[0]
         self.in_cells = self.inn['cells']
         self.in_masks = np.array([c['mask_im'] for c in self.in_cells])
+        self.mask_shape = self.in_masks[0].shape
         self.in_masks_infov = np.array([c['mask_im'] for c in self.in_cells if c['was_in_fov']])
         self.out_masks = self.out['A'].toarray()
         n_outmasks = self.out_masks.shape[-1]
@@ -98,13 +97,73 @@ class Result(object):
         self.in_cell_neighbors=np.array([self.n_neighbors(c) for c in self.in_cells] )
         self.in_cell_match_quality = [self.match_quality(idx) for idx,c in enumerate(self.in_cells)]
         self.out_cell_max_ca = np.max(self.out['C'],axis=1)
+        self.deconv_f = self.out['f']
         self.out_max_npil = np.max(self.out['f'])
         self.out_psn = self.out['P'][0][0][0].reshape(self.in_masks.shape[1:])
+        self.deconv_psn = self.out['P'][0][0][0].reshape(self.in_masks.shape[1:])
 
         self.n_in_cells_matched = len(np.unique(self.matches[:,self.IN])) 
         self.percent_in_cells_matched = float(self.n_in_cells_matched)/float(len(self.in_masks_infov))
         self.variances = [variance(self.out['C'][m[self.OUT]], self.in_cells[m[self.IN]]['ca']) for m in self.matches]
-        
+
+    def get_inmask(self):
+        return np.sum(self.in_masks, axis=0)
+    def get_outmask(self):
+        return np.sum(self.out_masks, axis=0)
+
+    def get_input_cell_summaries(self):
+        cell_dtype = np.dtype([ ('input_path', 'a%i'%(len(self.in_path))),\
+                                ('output_path', 'a%i'%(len(self.out_path))),\
+                                ('expression', np.float32),\
+                                ('offset', np.float32),\
+                                ('matched', np.bool),\
+                                ('sim_npil_mag', np.float32),\
+                                ('sim_noise_g_std', np.float32),\
+                                ('sim_noise_sh_mag', np.float32),\
+                                ('sim_n_cells', np.int8),\
+                              ])
+        cells = np.zeros(len(self.inn['cells']), dtype=cell_dtype)
+        for ci,c in enumerate(self.inn['cells']):
+            cells[ci]['input_path'] = self.in_path
+            cells[ci]['output_path'] = self.out_path
+            cells[ci]['expression'] = float(c['expression'])
+            cells[ci]['offset'] = float(c['offset'])
+            cells[ci]['matched'] = ci in np.unique(self.matches[:,self.IN])
+            cells[ci]['sim_npil_mag'] = float(self.inn_params['neuropil_mag'])
+            cells[ci]['sim_noise_g_std'] = float(self.inn_params['imaging_noise'][1])
+            cells[ci]['sim_noise_sh_mag'] = float(self.inn_params['imaging_noise_mag'])
+            cells[ci]['sim_n_cells'] = int(self.inn_params['n_cells_in_fov'])
+        return cells
+    def get_output_cell_summaries(self):
+        cell_dtype = np.dtype([ ('input_path', 'a%i'%(len(self.in_path))),\
+                                ('output_path', 'a%i'%(len(self.out_path))),\
+                                ('input_match_idx', np.int8),\
+                                ('match_perc_in', np.float32),\
+                                ('match_perc_out', np.float32),\
+                                ('C', np.float32, self.out['C'].shape[1]),\
+                                ('deconv_f', np.float32, self.deconv_f.shape),\
+                                ('deconv_mean_psn', np.float32),\
+                                ('sim_npil_mag', np.float32),\
+                                ('sim_noise_g_std', np.float32),\
+                                ('sim_noise_sh_mag', np.float32),\
+                                ('sim_n_cells', np.int8),\
+                              ])
+        cells = np.zeros(len(self.out_masks), dtype=cell_dtype)
+        for ci in xrange(len(self.out_masks)):
+            cells[ci]['input_path'] = self.in_path
+            cells[ci]['output_path'] = self.out_path
+            cells[ci]['input_match_idx'] = self.matches_all[ci,self.IN]
+            cells[ci]['match_perc_in'] = self.matches_all[ci,self.PERC_IN]
+            cells[ci]['match_perc_out'] = self.matches_all[ci,self.PERC_OUT]
+            cells[ci]['C'] = self.out['C'][ci]
+            cells[ci]['deconv_f'] = self.deconv_f
+            cells[ci]['deconv_mean_psn'] = np.mean(self.deconv_psn)
+            cells[ci]['sim_npil_mag'] = float(self.inn_params['neuropil_mag'])
+            cells[ci]['sim_noise_g_std'] = float(self.inn_params['imaging_noise'][1])
+            cells[ci]['sim_noise_sh_mag'] = float(self.inn_params['imaging_noise_mag'])
+            cells[ci]['sim_n_cells'] = int(self.inn_params['n_cells_in_fov'])
+        return cells
+
     def n_neighbors(self, c):
         th = 9
         n = 0
@@ -178,43 +237,28 @@ def parse_results(taskn):
     outname = pjoin('/jukebox/wang/deverett/tmp','temp_%i'%taskn)
 
     res = Result(inp, outp)
-    dic = {}
-    dic['input'] = inp
-    dic['output'] = outp
-    dic['pct_matched'] = res.percent_in_cells_matched
-    dic['avg_variance'] = np.mean(res.variances)
-    dic['n_cells'] = res.inn_params['n_cells_in_fov']
-    dic['noise_mag'] = res.inn_params['imaging_noise_mag']
-    dic['noise'] = res.inn_params['imaging_noise']
-    dic['timing_offset_std'] = res.inn_params['cell_timing_offset'][1]
-    dic['neuropil_mag'] = res.inn_params['neuropil_mag']
-    dic['in_cell_expressions'] = [c['expression'] for c in res.in_cells]
-    dic['in_cell_matched'] = [cidx in res.matches[:,res.IN] for cidx in xrange(len(res.inn['cells']))]
-    dic['in_cell_neighbors'] = res.in_cell_neighbors
-    dic['in_cell_match_quality'] = res.in_cell_match_quality
-    dic['out_cell_max_ca'] = res.out_cell_max_ca
-    dic['out_max_npil'] = res.out_max_npil
-    dic['avg_out_psn'] = np.mean(res.out_psn)
-    dic['std_out_psn'] = np.std(res.out_psn)
-
-    np.save(outname, np.array([dic]))
+    np.savez(outname, inn=res.get_input_cell_summaries(), out=res.get_output_cell_summaries(), in_mask=res.get_inmask(), out_mask=res.get_outmask())
 
 def merge_results():
-    outname = pjoin(sim_dir,'comparison.npy')
+    outname = pjoin(sim_dir,'comparison.npz')
     if os.path.exists(outname):
         os.remove(outname)
-    out_all = []
 
+    inn = []
+    out = []
     for f in sorted(os.listdir(temp_dir)):
         data = np.load(pjoin(temp_dir,f))
-        for d in data:
-            out_all.append(np.array([d]))
+        inn.append(data['inn'])
+        out.append(data['out'])
         os.remove(pjoin(temp_dir,f))
+    inn = np.concatenate(np.array(inn))
+    out = np.concatenate(np.array(out))
+
     for f in os.listdir(temp_dir):
         if f[0]=='.':
             os.remove(pjoin(temp_dir,f))
     os.rmdir(temp_dir)
-    np.save(outname, out_all)
+    np.savez(outname, inn=inn, out=out)
 
 def get_outpaths():
     return [pjoin(out_dir,subdir,simdir,simdir[:2]) for subdir in [dd for dd in os.listdir(out_dir) if os.path.isdir(pjoin(out_dir,dd))] for simdir in os.listdir(pjoin(out_dir,subdir)) if re.match('^[0-9]{2}_[0-9]{3}$',simdir)]
@@ -236,93 +280,6 @@ def count_available():
     print "Available output (deconvolution) files:\t\t\t%i"%(len(out_paths)) 
     print "Available pairs to analyze:\t\t\t\t%i"%(len(inout_paths)) 
 
-def figure1():
-    #neuropil
-    data = np.load(pjoin(sim_dir,'comparison.npy'))
-    data = np.array([d[0] for d in data])
-    data = data[np.argsort([d['input'] for d in data])]
-    
-    MA = 0
-    EX = 1
-    NP = 2
-    data_ma = [d['in_cell_matched'] for d in data]
-    data_ex = [d['in_cell_expressions'] for d in data]
-    data_np = [d['neuropil_mag'] for d in data]
-    data_all = []
-    for dm,de,dn in zip(data_ma,data_ex,data_np):
-        assert len(dm) == len(de)
-        for dmm,dee in zip(dm,de):
-            data_all.append([int(dmm), float(dee), float(dn)])
-    data_all = np.array(data_all)
-    data_no = data_all[np.squeeze(np.argwhere(data_all[:,MA]==0)),:]
-    data_yes = data_all[np.squeeze(np.argwhere(data_all[:,MA]==1)),:]
-
-    #bin means
-    np_vals = np.unique(data_all[:,NP])
-    data_no = np.array([data_no[np.argwhere(data_no[:,NP]==nv)] for nv in np_vals])
-    data_yes = np.array([data_yes[np.argwhere(data_yes[:,NP]==nv)] for nv in np_vals])
-    data_no_mean = np.array([np.mean(i) for i in data_no])
-    data_yes_mean = np.array([np.mean(i) for i in data_yes])
-    data_no_std = np.array([np.std(i) for i in data_no])
-    data_yes_std = np.array([np.std(i) for i in data_yes])
-    data_no_sem = np.array([np.std(i)/np.sqrt(len(i)) for i in data_no])
-    data_yes_sem = np.array([np.std(i)/np.sqrt(len(i)) for i in data_yes])
-    mult = 180.
-    s_no = np.array([len(nn)/float(len(nn)+len(yy)) for nn,yy in zip(data_no,data_yes)])
-    s_yes = np.array([1-sn for sn in s_no])
-    s_no,s_yes = mult*np.array([s_no,s_yes])
-
-    pl.scatter(np_vals,data_no_mean,marker='o',label='Unfound Cells', color='r', s=s_no)
-    pl.scatter(np_vals,data_yes_mean,marker='o',label='Found Cells', color='g',s=s_yes)
-    pl.errorbar(np_vals,data_no_mean,yerr=data_no_sem,fmt=None,ecolor='k')
-    pl.errorbar(np_vals,data_yes_mean, yerr=data_yes_sem,fmt=None,ecolor='k')
-    pl.legend(loc='upper left',numpoints=1)
-    
-    #next
-    data_np_full = cat(data_np, float)
-    pct_matched = cat([d['pct_matched'] for d in data], float)
-    
-    pl.figure(2)
-    idxs = np.argsort(data_np_full)
-    pl.scatter(data_np_full[idxs],pct_matched[idxs])
-
-
-    pl.figure(3)
-    #make the curve of yes/no matched vs expression level
-    data_ex = cat(data_ex, float)
-    rang = np.linspace(data_ex.min(),data_ex.max(),50)
-    data_ma = cat(data_ma, int)
-    ma_binned = []
-    for r1,r0 in zip(rang[1:],rang[:-1]):
-        ma_binned.append(np.mean(data_ma[np.argwhere(np.logical_and(data_ex<r1, data_ex>=r0))]))
-    ma_binned.append(np.mean(data_ma[np.argwhere(data_ex>rang[-1])]))
-    pl.scatter(rang,ma_binned) 
-    
-    #from sims:
-    CA = 0
-    NP = 1
-    data_out_ca = [d['out_cell_max_ca'] for d in data]
-    data_out_npil = [d['out_max_npil'] for d in data]
-    data_out = []
-    for dc,dn in zip(data_out_ca, data_out_npil):
-        for dcc in dc:
-            data_out.append([float(dcc), float(dn)])
-    data_out = np.array(data_out)
-    np_vals = np.unique(data_out[:,NP])
-    data_out = np.array([data_out[np.argwhere(data_out[:,NP]==nv)] for nv in np_vals])
-    data_out_mean = np.array([np.mean(i) for i in data_out])
-    data_out_sem = np.array([np.std(i)/np.sqrt(len(i)) for i in data_out])
-    pl.figure(4)
-    pl.plot(np_vals,data_out_mean,marker='o',markersize=7,linestyle='None', color='k')
-    pl.errorbar(np_vals,data_out_mean,yerr=data_out_sem,fmt=None,ecolor='gray')
-
-    #noise p.sn
-    dd = cat([d['avg_out_psn'] for d in data],float)
-    pl.figure(5)
-    pl.plot(dd,'b*')
-    
-    return data_all
-
 if __name__ == '__main__':
     option = sys.argv[1]
     if option == 'merge':
@@ -332,35 +289,79 @@ if __name__ == '__main__':
     elif option == 'parse':
         taskn = int(sys.argv[2])-1
         parse_results(taskn)
-    elif option == 'figure1':
-        data_all=figure1()
+    elif option == 'figure':
+        data = np.load(pjoin(sim_dir,'comparison.npz'))
+        inn,out = data['inn'],data['out']
+        inn = inn[np.argsort(inn['input_path'])]
+        out = out[np.argsort(out['output_path'])]
+        
+        figidx = int(sys.argv[2])
+
+        if figidx == 1:
+            #batch3
+            unique_npil_mags = np.unique(inn['sim_npil_mag'])
+            yes_idxs = [np.argwhere(np.logical_and(inn[:]['matched']==True, inn[:]['sim_npil_mag']==nm)) for nm in unique_npil_mags]
+            no_idxs = [np.argwhere(np.logical_and(inn[:]['matched']==False, inn[:]['sim_npil_mag']==nm)) for nm in unique_npil_mags]
+            yes_expr,no_expr = zip(*[[inn['expression'][yi],inn['expression'][ni]] for yi,ni in zip(yes_idxs,no_idxs)])
+            yes_ns,no_ns = map(len,yes_expr),map(len,no_expr)
+            yes_means,no_means = map(np.mean, yes_expr),map(np.mean, no_expr)
+            yes_stds,no_stds = map(np.std, yes_expr),map(np.std, no_expr)
+            yes_sems, no_sems = zip(*[[ys/np.sqrt(yn), ns/np.sqrt(nn)] for ys,yn,ns,nn in zip(yes_stds,yes_ns,no_stds,no_ns)])
+            mult = 180.
+            yes_szs,no_szs = zip(*[[mult*yn/(yn+nn), mult*nn/(yn+nn)] for yn,nn in zip(yes_ns,no_ns)])
+
+            pl.scatter(unique_npil_mags,no_means,marker='o',label='Unfound Cells', color='r', s=no_szs)
+            pl.scatter(unique_npil_mags,yes_means,marker='o',label='Found Cells', color='g', s=yes_szs)
+            pl.errorbar(unique_npil_mags,no_means,yerr=no_sems,fmt=None,ecolor='k')
+            pl.errorbar(unique_npil_mags,yes_means,yerr=yes_sems,fmt=None,ecolor='k')
+            pl.legend(loc='upper left',numpoints=1)
+            pl.xlabel('Neuropil Magnitude', fontsize=20)
+            pl.ylabel('Cell Expression', fontsize=20)
+
+        elif figidx == 2:
+            #batch3
+            idxs = np.argsort(inn['expression'])
+            bins = np.floor(np.linspace(0,len(idxs)+1,30))
+            lims = [(i1,i2) for i1,i2 in zip(bins[:-1], bins[1:])]
+            expr = [np.mean(inn['expression'][idxs][i1:i2]) for i1,i2 in lims]
+            pct_matched = [np.mean(inn['matched'][idxs][i1:i2]) for i1,i2 in lims]
+            pl.scatter(expr, pct_matched)
+            pl.xlabel('Cell Expression', fontsize=20)
+            pl.ylabel('Fraction of Cells Matched', fontsize=20)
+        
+        elif figidx == 3:
+            #batches 1,3,4
+            try:
+                op = sys.argv[3]
+            except:
+                print "Specify whether to plot pct matched (pct) or p.sn (psn)."
+                sys.exit(0)
+            if 'batch_2' in sim_dir:
+                vname = 'sim_noise_sh_mag'
+            elif 'batch_3' in sim_dir:
+                vname = 'sim_npil_mag'
+            elif 'batch_4' in sim_dir:
+                vname = 'sim_noise_g_std'
+            unique_var_mags = np.unique(inn[vname])
+            if op == 'pct':
+                outcome = [np.mean(inn['matched'][np.argwhere(inn[vname]==vm)]) for vm in unique_var_mags]
+            elif op == 'psn':
+                outcome = [np.mean(out['deconv_mean_psn'][np.argwhere(inn[vname]==vm)]) for vm in unique_var_mags]
+            pl.scatter(unique_var_mags, outcome)
+            pl.xlabel(vname, fontsize=20)
+            pl.ylabel(op, fontsize=20)
+       
+        elif figidx == 4:
+            try:
+                op = int(sys.argv[3])
+            except:
+                print "Specify: for cells with expression greater than __"
+                sys.exit(0)
+            unique_var_mags = np.unique(inn['sim_noise_g_std'])
+            outcome = [np.mean(inn['matched'][np.argwhere(np.logical_and(inn['sim_noise_g_std']==vm, inn['expression']>op))]) for vm in unique_var_mags]
+            pl.scatter(unique_var_mags, outcome)
+            pl.xlabel('Noise Magnitude', fontsize=20)
+            pl.ylabel('Fraction of Cells Matched', fontsize=20)
+        
     elif option == 'play':
-        data = np.load(pjoin(sim_dir,'comparison.npy'))
-        data = np.array([d[0] for d in data])
-        data = data[np.argsort([d['input'] for d in data])]
-        
-        #batch4
-        data_ex = cat([d['in_cell_expressions'] for d in data],float)
-        idxs_filt = np.argwhere(data_ex>-1.)
-        data_ex_filt = data_ex[idxs_filt]
-        data_ma = cat([d['in_cell_matched'] for d in data],int)
-        data_ma_filt = np.squeeze(data_ma[idxs_filt])
-        data_noise = np.array([d['noise'][1] for d in data])
-        data_noise = np.repeat(data_noise, float(len(data_ma))/len(data_noise))
-        data_noise_filt = np.squeeze(data_noise[idxs_filt])
-        idxs = np.argsort(data_noise_filt) 
-        lims = [(i1,i2) for i1,i2 in zip(np.arange(0,len(idxs),100)[:-1],np.arange(0,len(idxs),100)[1:])]
-        data_ma_bin = [np.mean(data_ma_filt[idxs][i1:i2]) for i1,i2 in lims]
-        data_noise_bin = [np.mean(data_noise_filt[idxs][i1:i2]) for i1,i2 in lims]
-        pl.scatter(data_noise_bin, data_ma_bin)
-        
-        sys.exit(0)
-        xaxis = 'noise'
-        xtype = float
-        yaxis = 'pct_matched'
-        ytype = float
-        
-        datax = cat([d[xaxis][1] for d in data], xtype)
-        datay = cat([d[yaxis] for d in data], ytype)
-        idxs = np.argsort(datax)
-        pl.scatter(datax[idxs],datay[idxs])
+        pass
